48a21de4a670386113610d39fb0ae9c29f3e2b80
Pornpat Santibuppakul <dottypurkt@gmail.com>
2025-03-30 01:36:34 +0700

    Make MockMyLogger class private

--- core/testing/src/main/kotlin/com/purkt/mindexpense/core/testing/logging/TestMyLogger.kt
+++ core/testing/src/main/kotlin/com/purkt/mindexpense/core/testing/logging/TestMyLogger.kt
@@ -3,7 +3,11 @@ package com.purkt.mindexpense.core.testing.logging
 import com.purkt.mindexpense.core.logging.MyLogger
 import org.koin.dsl.module
 
-class MockMyLogger: MyLogger {
+val mockMyLoggerModule = module {
+    single<MyLogger> { MockMyLogger() }
+}
+
+private class MockMyLogger: MyLogger {
     override fun i(tag: String?, message: String, vararg args: Any?) {}
     override fun i(e: Throwable?, tag: String?) {}
     override fun v(tag: String?, message: String, vararg args: Any?) {}
@@ -14,8 +18,4 @@ class MockMyLogger: MyLogger {
     override fun w(e: Throwable?, tag: String?) {}
     override fun e(tag: String?, message: String, vararg args: Any?) {}
     override fun e(e: Throwable?, tag: String?) {}
-}
-
-val mockMyLoggerModule = module {
-    single<MyLogger> { MockMyLogger() }
 }
